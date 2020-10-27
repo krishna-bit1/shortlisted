cff6fd6d42ce6d58293f9238696867d082830b46
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-10-27 12:02:49 +0500

    Binary Search Tree Algorithm Added

---
+++ BinarySearchTree/src/binarysearchtree/B1.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearchtree;
+
+/**
+ *
+ * @author Ali
+ */
+public class B1 {
+
+    
+    
+    class Node { 
+        int key; 
+        Node left, right; 
+  
+        public Node(int item) { 
+            key = item; 
+            left = right = null; 
+        } 
+    } 
+  
+    // Root of BST 
+    public Node root; 
+  
+    // Constructor 
+    B1() {  
+        root = null;  
+    } 
+  
+    // This method mainly calls insertRec() 
+    void insert(int key) { 
+       root = insertRec(root, key); 
+    } 
+      
+    /* A recursive function to insert a new key in BST */
+    Node insertRec(Node root, int key) { 
+  
+        /* If the tree is empty, return a new node */
+        if (root == null) { 
+            root = new Node(key); 
+            return root; 
+        } 
+  
+        /* Otherwise, recur down the tree */
+        if (key < root.key) 
+            root.left = insertRec(root.left, key); 
+        else if (key > root.key) 
+            root.right = insertRec(root.right, key); 
+  
+        /* return the (unchanged) node pointer */
+        return root; 
+    } 
+  
+    // This method mainly calls InorderRec() 
+    void inorder()  { 
+       inorderRec(root); 
+    } 
+  
+    // A utility function to do inorder traversal of BST 
+    void inorderRec(Node root) { 
+        if (root != null) { 
+            inorderRec(root.left); 
+            System.out.print(root.key+" "); 
+            inorderRec(root.right); 
+        } 
+    }
+   
+}
